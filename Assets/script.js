2829257dd983fa2e206ab9aec201f3be1c2dd6a6
Elijah B <69051669+elijah415hz@users.noreply.github.com>
2020-09-08 20:33:33 -0700

    Fully hide length card, length button prototype functioning

--- Assets/script.js
+++ Assets/script.js
@@ -1,24 +1,38 @@
 // Set form to invisible on page load
-// window.onload = function() {
+window.onload = function() {
 
-//   document.getElementById("getLength").style.display = "none";
+  document.getElementById("getLength").style.display = "none";
 
-// };
+};
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var lengthBtn = document.querySelector("#submitLength")
 
 // Write password to the #password input
 function writePassword() {
-  document.getElementsById("getLength").style.display = "block";
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
+  
   passwordText.value = password;
 
 }
 
 function generatePassword() {
+  document.getElementById("getLength").style.display = "block";
 }
-// Add event listener to generate button
+
+function logLength() {
+  var length = document.getElementById("length").value;
+  if ((length < 8) | (length > 128)) {
+    alert("Length must be no less than 8 and no greater than 128");
+    length = undefined;
+    generatePassword()
+  } else {
+    console.log(length)
+}
+}
+// Add event listeners to buttons
 generateBtn.addEventListener("click", writePassword);
+lengthBtn.addEventListener("click", logLength);
+// 
