2715a27fa0c2d7f7774b14da991bd8d6c291d431
Elijah B <69051669+elijah415hz@users.noreply.github.com>
2020-09-09 10:01:53 -0700

    added logChars

--- Assets/script.js
+++ Assets/script.js
@@ -1,13 +1,23 @@
 // Set form to invisible on page load
+
 window.onload = function() {
+  hideById("getLength")
+  hideById("chooseChars")
+}
 
-  document.getElementById("getLength").style.display = "none";
 
-};
+function hideById(id) {
+  document.getElementById(id).style.display = "none";
+}
+
+function showById(id) {
+  document.getElementById(id).style.display = "block";
+}
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var lengthBtn = document.querySelector("#submitLength")
+var lengthBtn = document.querySelector("#submitLength");
+var charBtn = document.querySelector("#submitChars");
 
 // Write password to the #password input
 function writePassword() {
@@ -19,20 +29,37 @@ function writePassword() {
 }
 
 function generatePassword() {
-  document.getElementById("getLength").style.display = "block";
+  showById("getLength")
 }
 
 function logLength() {
   var length = document.getElementById("length").value;
-  if ((length < 8) | (length > 128)) {
+  if ((length > 7) & (length < 129)) {
+    console.log(length)
+    hideById("getLength")
+    showById("chooseChars")
+  } else {
     alert("Length must be no less than 8 and no greater than 128");
     length = undefined;
-    generatePassword()
+    document.getElementById("length").value = undefined;
+}
+}
+
+function logChars() {
+  var lower = document.getElementById("lower").checked;
+  var upper = document.getElementById("upper").checked;
+  var num = document.getElementById("num").checked;
+  var special = document.getElementById("special").checked;
+  if (lower | upper | num | special) {
+    hideById("chooseChars")
+    // Do the next thing!
   } else {
-    console.log(length)
+    alert("You must select at least one character type")
+  }
 }
-}
+
 // Add event listeners to buttons
 generateBtn.addEventListener("click", writePassword);
 lengthBtn.addEventListener("click", logLength);
+charBtn.addEventListener("click", logChars);
 // 
